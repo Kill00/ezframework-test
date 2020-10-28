a7a33d1ea803a002107514e21b40f2520e7c3398
Kill00 <hellow1211other@gmail.com>
2020-10-28 15:40:22 +0900

    Fix

---
+++ src/main/java/com/teamvery/ezframeworktest/main.java
@@ -0,0 +1,13 @@
+package com.teamvery.ezframeworktest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import me.kingcjy.ezframework.EzFramework;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        EzFramework.run(this);
+
+    }
+}
